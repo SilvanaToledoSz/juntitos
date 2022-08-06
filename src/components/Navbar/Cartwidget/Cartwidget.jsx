b2d1d3f421214dd5788d8bd76f94f2d0146b71d1
SilvanaToledoSz <silvana.toledo.sz@gmail.com>
2022-08-06 17:18:33 -0300

    carga de segundo y tercer desafío entregable

--- src/components/Navbar/Cartwidget/Cartwidget.jsx
+++ src/components/Navbar/Cartwidget/Cartwidget.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Cartwidget = () => {
   return (
     <div>
-      <div style={{backgroundColor:'#424251', color:'#F7F7F7'}} className="p-2 rounded me-4"><i style={{color:'#F7F7F7'}} class="fa-solid fa-cart-shopping"></i>0</div>
+      <div style={{backgroundColor:'#424251', color:'#F7F7F7'}} className="p-2 rounded me-4"><i style={{color:'#F7F7F7'}} className="fa-solid fa-cart-shopping"></i>0</div>
     </div>
   )
 }
